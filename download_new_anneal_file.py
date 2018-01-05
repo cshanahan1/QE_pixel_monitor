cdebba37a32fcb4572c2553f5e2bd8190c989f4e
Clare Shanahan <cshanahan@stsci.edu>
2018-01-04 23:49:46 -0500

    changes

--- download_new_anneal_file.py
+++ download_new_anneal_file.py
@@ -1,15 +1,15 @@
 import requests
 from paths_and_params import paths
 
-def download_updated_anneal_file(anneal_info_dir):
+def download_updated_anneal_file(:
 	url = 'http://www.stsci.edu/hst/wfc3/ins_performance/monitoring/UVIS/anneal_dates-tab.txt'
 	r = requests.get(url, allow_redirects = True)
-	open(anneal_info_dir+'/anneal_dates.txt','w').write(r.content)
+	open('anneal_dates.txt','w').write(r.content)
 	print 'Downloaded updated anneal dates file.'
 
-def parse_updated_anneal_file(anneal_info_dir):
+def parse_updated_anneal_file():
 
-	anneal_file = anneal_info_dir + '/anneal_dates.txt'
+	anneal_file = 'anneal_dates.txt'
 	
 	anneal_mjds = []
 	with open(anneal_file,'r') as f:
@@ -21,19 +21,18 @@ def parse_updated_anneal_file(anneal_info_dir):
 					anneal_mjds.append(line_split[0])
 	return anneal_mjds
 	
-def write_mjd_to_file(anneal_info_dir,anneal_mjds):
+def write_mjd_to_file(anneal_mjds):
 
-	with open(anneal_info_dir+'/anneal_mjds.txt','w') as f:
+	with open('anneal_mjds.txt','w') as f:
 		for mjd in anneal_mjds:
 			f.write(mjd+'\n')
 			
 def main_update_anneal_file(anneal_info_dir):
-	download_updated_anneal_file(anneal_info_dir)
-	anneal_mjds = parse_updated_anneal_file(anneal_info_dir)
-	write_mjd_to_file(anneal_info_dir,anneal_mjds)
+	download_updated_anneal_file()
+	anneal_mjds = parse_updated_anneal_file()
+	write_mjd_to_file(anneal_mjds)
 	
 			
 if __name__ == '__main__':
-	anneal_info_dir = paths()['anneal_info_dir']
-	main_update_anneal_file(anneal_info_dir)
-	
+
+	main_update_anneal_file()	
